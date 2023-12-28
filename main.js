2acb29549bcdfe49f35d4ecddaf9e6d24c84e203
Seolmisae <seolmisae@gmail.com>
2023-12-28 18:20:12 +0900

    3rd

--- main.js
+++ main.js
@@ -66,7 +66,7 @@ var app = http.createServer(function (request, response) {
               title,
               list,
               `<h2>${title}</h2>${description}`,
-              `<a href="/create">create</a><a href="/update"></a>`
+              `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
             );
 
             response.writeHead(200);
@@ -82,15 +82,16 @@ var app = http.createServer(function (request, response) {
       var template = templateHTML(
         title,
         list,
-        `<form action="http://localhost:3000/create_process" method="post">
-        <p><input type="text" name="title" placeholder="title"/></p>
-        <p>
-          <textarea name="description" placeholder="description"></textarea>
-        </p>
-        <p>
-          <input type="submit" />
-        </p>
-      </form>
+        `
+          <form action="/create_process" method="post">
+            <p><input type="text" name="title" placeholder="title"/></p>
+            <p>
+              <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+              <input type="submit" />
+            </p>
+          </form>
       `,
         ""
       );
@@ -111,8 +112,35 @@ var app = http.createServer(function (request, response) {
       var description = post.description;
 
       fs.writeFile(`data/${title}`, description, "utf8", function (err) {
-        response.writeHead(302, { Location: `/?id=${encode(title)}` });
+        response.writeHead(302, { Location: `/?id=${title}` });
         response.end();
+      });
+    });
+  } else if (pathname === "/update") {
+    fs.readdir("./data", function (error, filelist) {
+      fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
+        var title = queryData.id;
+        var list = templateList(filelist);
+        var template = templateHTML(
+          title,
+          list,
+          `
+            <form action="/update_process" method="post">
+              <input type="hidden" name="id" value="${title}">
+              <p><input type="text" name="title" placeholder="title" value="${title}"/></p>
+              <p>
+                <textarea name="description" placeholder="description">${description}</textarea>
+              </p>
+              <p>
+                <input type="submit" />
+              </p>
+            </form>
+        `,
+          `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+        );
+
+        response.writeHead(200);
+        response.end(template);
       });
     });
   } else {
